c57fadb76992146b5cce83d1738d91a1fb3e2b70
Oleksiy12 <betbonnay@gmail.com>
2023-05-17 14:50:55 +0300

    update5

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,20 +1,37 @@
-// import throttle from 'lodash';
-import Player from '@vimeo/player';
-// const player = new Player("vimeo-player");
+// import Player from '@vimeo/player';
+// import { result } from 'lodash';
+// const iframe = document.getElementById("vimeo-player");
+
+// const player = new Player(iframe);
+   
+// player.on('play', onPlay);
+
+// function onPlay(event){
+//     let result = event.seconds;
+//     console.log(result);
 
 
-// const saveCurrentTime = (time) => {
-//     localStorage.setItem('videoplayer-current-time', time);
-//   };
-//   player.on('timeupdate', throttle(() => {
-//     const currentTime = player.currentTime();
-//     saveCurrentTime(currentTime);
-//   }, 1000));
 
-//   const savedTime = localStorage.getItem('videoplayer-current-time');
-// if (savedTime) {
-//   player.setCurrentTime(parseFloat(savedTime));
 // }
 
-const iframe = document.querySelector("iframe");
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+// Створення екземпляру плеєра
+const playerElement = document.getElementById('vimeo-player');
+const player = new Player(playerElement, {
+  url: 'https://player.vimeo.com/video/236203659'
+});
+
+// Отримання та збереження часу відтворення
+player.on('timeupdate', throttle(() => {
+  player.getCurrentTime().then((seconds) => {
+    localStorage.setItem('videoplayer-current-time', seconds);
+  });
+}, 1000));
+
+// Відновлення відтворення зі збереженої позиції
+const savedTime = localStorage.getItem('videoplayer-current-time');
+if (savedTime) {
+  player.setCurrentTime(savedTime);
+}
